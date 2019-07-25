b1801e85f99c290e63b66223736b41a50a5985a1
adamRajmuller <rajfta@gmail.com>
2019-07-25 15:00:26 +0200

    checkout 1.0.0

--- src/main/java/com/codecool/shop/controller/Payment.java
+++ src/main/java/com/codecool/shop/controller/Payment.java
@@ -3,10 +3,12 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.OrderDaoMem;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.OrderedItems;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -17,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet(urlPatterns = {"/payment"})
@@ -43,6 +46,20 @@ public class Payment extends HttpServlet {
         params.put("suppliers", productDataStore.getBy(productCategoryDataStore.find(1)));
         context.setVariables(params);
          */
+        engine.process("product/payment.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List<OrderedItems> orderedItems = OrderDaoMem.getInstance().getAll();
+        OrderDaoMem orderedItemDataStore = OrderDaoMem.getInstance();
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+
+        float summPriceInt = orderedItemDataStore.getPriceForAll();
+        context.setVariable("summPrice", summPriceInt);
         engine.process("product/payment.html", context, resp.getWriter());
     }
 
